15ad884cbb32a52198844dd36c2d5fb6359d48b7
Peter Doherty <pdoherty@protonmail.com>
2019-12-07 15:57:28 -0500

    credit build.rs "inspiration"

--- build.rs
+++ build.rs
@@ -1,3 +1,6 @@
+// Copied mostly verbatim from:
+// https://doc.rust-lang.org/cargo/reference/build-scripts.html
+
 use std::env;
 use std::path::Path;
 use std::process::Command;
@@ -5,8 +8,6 @@ use std::process::Command;
 fn main() {
     let out_dir = env::var("OUT_DIR").unwrap();
 
-    // note that there are a number of downsides to this approach, the comments
-    // below detail how to improve the portability of these commands.
     Command::new("gcc")
         .args(&["src/lib/greet.c", "-c", "-fPIC", "-o"])
         .arg(&format!("{}/greet.o", out_dir))
