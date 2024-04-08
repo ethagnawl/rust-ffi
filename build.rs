427815f5b9fcb8993968f5bc24e62c0a2888eaa7
Pete Doherty <pdoherty+git@protonmail.com>
2024-04-08 10:45:25 -0400

    use cc library in build script to greatly simplify and make workflow portable

--- build.rs
+++ build.rs
@@ -1,24 +1,7 @@
 // Copied mostly verbatim from:
 // https://doc.rust-lang.org/cargo/reference/build-scripts.html
 
-use std::env;
-use std::path::Path;
-use std::process::Command;
-
 fn main() {
-    let out_dir = env::var("OUT_DIR").unwrap();
-
-    Command::new("gcc")
-        .args(&["src/lib/greet.c", "-c", "-fPIC", "-o"])
-        .arg(&format!("{}/greet.o", out_dir))
-        .status()
-        .unwrap();
-    Command::new("ar")
-        .args(&["cvq", "libgreet.a", "greet.o"])
-        .current_dir(&Path::new(&out_dir))
-        .status()
-        .unwrap();
-
-    println!("cargo:rustc-link-search=native={}", out_dir);
-    println!("cargo:rustc-link-lib=static=greet");
+    cc::Build::new().file("src/lib/greet.c").compile("greet");
+    println!("cargo:rerun-if-changed=src/lib/greet.c");
 }
